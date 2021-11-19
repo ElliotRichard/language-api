373c4fad8b016d5e91ec21e5d5c348e95d91d99f
DickElliot <elliotrichard@protonmail.com>
2021-11-19 18:37:19 +1300

    Configured for Heroku deployment

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import scraping
 
 app = Flask(__name__)
 api = Api (app)
+CORS(app)
 
 class LanguageCode(Resource):
   def get(self, language):
@@ -15,4 +17,6 @@ api.add_resource(LanguageCode, '/','/language/<string:language>')
   
 if __name__ == '__main__':
   print("Starting the language code API")
-  app.run(debug=True)
+  app.run()
+  # Development Mode
+  # app.run(debug=True)
